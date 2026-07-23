a603a540c8ca1bbdc5ecaf49f5d96eef79f0a247
Jose Pablo Peralta Torres <pablotorres0895@gmail.com>
2026-07-23 23:14:18 +0000

    refactor (fix) adapt to code provided for the example

--- models/marts/finance/fct_orders.sql
+++ models/marts/finance/fct_orders.sql
@@ -1,14 +1,30 @@
-with orders as (
-    select * from {{ ref('stg_jaffle_shop_orders') }}
+with orders as  (
+    select * from {{ ref ('stg_jaffle_shop_orders' )}}
 ),
+
 payments as (
-    select * from {{ ref('stg_stripe__payments') }}
+    select * from {{ ref ('stg_stripe__payments') }}
+),
+
+order_payments as (
+    select
+        order_id,
+        sum (case when payment_status = 'success' then payment_amount end) as amount
+
+    from payments
+    group by 1
+),
+
+ final as (
+
+    select
+        orders.order_id,
+        orders.customer_id,
+        orders.order_date,
+        coalesce (order_payments.amount, 0) as amount
+
+    from orders
+    left join order_payments using (order_id)
 )
 
-select 
-    o.order_id,
-    o.customer_id,
-    p.amount
-from 
-    orders o join payments p 
-        on o.order_id = p.orderid
+select * from final
